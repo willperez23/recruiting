63b5519ae2d563c4e7bd0de6ce2bb801e73722a5
willperez23 <william.perez.23@dartmouth.edu>
2025-01-25 09:55:14 -0800

    routin

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
-import $ from 'jquery';
-import './style.scss';
-// $('#main').html('Here we go!');
-let num = 0;
-setInterval(() => { $('#main').html(`Youve been on this page for ${num} seconds.`); num += 1; }, 1000);
+// import $ from 'jquery';
+// import './style.scss';
+// // $('#main').html('Here we go!');
+// let num = 0;
+// setInterval(() => { $('#main').html(`Youve been on this page for ${num} seconds.`); num += 1; }, 1000);
