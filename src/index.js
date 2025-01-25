a5d2291fdf05b6cdbb82ff6f295da54b3fbc71a0
willperez23 <william.perez.23@dartmouth.edu>
2025-01-24 16:48:24 -0800

    basic set up complete

--- src/index.js
+++ src/index.js
@@ -1,1 +1,5 @@
-console.log('starting up!');
+import $ from 'jquery';
+import './style.scss';
+// $('#main').html('Here we go!');
+let num = 0;
+setInterval(() => { $('#main').html(`Youve been on this page for ${num} seconds.`); num += 1; }, 1000);
